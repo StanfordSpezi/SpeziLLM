cdb119b37cd3a8cd8dca3935b682d6cbd0eb2d7c
Adrit Rao <adrit.rao@gmail.com>
2023-10-05 21:43:18 -0700

    New Voice Interaction

--- Sources/SpeziOpenAI/MessageInputView.swift
+++ Sources/SpeziOpenAI/MessageInputView.swift
@@ -8,16 +8,22 @@
 
 import OpenAI
 import SwiftUI
+import AVFoundation
+import Speech
 
 
 /// Displays a textfield to append a message to a chat.
 public struct MessageInputView: View {
     let messagePlaceholder: String
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    private let audioEngine = AVAudioEngine()
     
     @Binding var chat: [Chat]
     @State var message: String = ""
     @State var messageViewHeight: CGFloat = 0
-    
+    @State private var isRecording = false
+    @State private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    @State private var recognitionTask: SFSpeechRecognitionTask?
     
     public var body: some View {
         HStack(alignment: .bottom) {
@@ -38,6 +44,9 @@ public struct MessageInputView: View {
                 .lineLimit(1...5)
             Button(
                 action: {
+                    audioEngine.stop()
+                    recognitionRequest?.endAudio()
+                    isRecording = false
                     chat.append(Chat(role: .user, content: message))
                     message = ""
                 },
@@ -54,6 +63,45 @@ public struct MessageInputView: View {
                 .padding(.trailing, -38)
                 .padding(.bottom, 3)
                 .disabled(message.isEmpty)
+            Button(action: {
+                if isRecording {
+                    audioEngine.stop()
+                    recognitionRequest?.endAudio()
+                    isRecording = false
+                } else {
+                    isRecording = true
+                    do {
+                        try startUpdatingMessage()
+                    } catch {
+                        
+                    }
+                }
+            }) {
+                ZStack {
+                    Circle()
+                        .foregroundColor(isRecording ? Color.red : Color.blue)
+                        .frame(width: 44, height: 44)
+                    
+                    Image(systemName: "mic.fill")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .frame(width: 44, height: 44)
+                        .background(Color.clear)
+                        .alignmentGuide(HorizontalAlignment.center, computeValue: { dimension in
+                            dimension[HorizontalAlignment.center]
+                        })
+                        .alignmentGuide(VerticalAlignment.center, computeValue: { dimension in
+                            dimension[VerticalAlignment.center]
+                        })
+                        .scaleEffect(isRecording ? 1.2 : 1.0)
+                        .opacity(isRecording ? 0.7 : 1.0)
+                        .animation(
+                            isRecording ? .easeInOut(duration: 0.5).repeatForever(autoreverses: true) : .default,
+                            value: isRecording
+                        )
+                }
+            }
+            .padding(EdgeInsets(top: 5, leading: 20, bottom: 0, trailing: 0))
         }
             .padding(.trailing, 23)
             .padding(.horizontal, 16)
@@ -74,7 +122,78 @@ public struct MessageInputView: View {
             .messageInputViewHeight(messageViewHeight)
     }
     
+    private func startUpdatingMessage() {
+        
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("Error setting up the audio session: \(error.localizedDescription)")
+        }
+        let inputNode = audioEngine.inputNode
+
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            
+            if isRecording {
+                if let result = result {
+                    
+                    if result.bestTranscription.formattedString.contains("send") {
+                        audioEngine.stop()
+                        recognitionRequest.endAudio()
+                        isRecording = false
+                        chat.append(Chat(role: .user, content: message))
+                        message = ""
+                    } else {
+                        message = result.bestTranscription.formattedString
+                    }
+                    
+                    isFinal = result.isFinal
+                    
+                }
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        }
+
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do { try audioEngine.start()
+        } catch {
+            print("Error setting up the audio session: \(error.localizedDescription)")
+        }
+    }
     
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            
+        } else {
+           
+        }
+    }
+
     /// - Parameters:
     ///   - chat: The chat that should be appended to.
     ///   - messagePlaceholder: Placeholder text that should be added in the input field
