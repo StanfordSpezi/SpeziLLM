07212e83db31bbb0f017d124e5d7a726508b9908
Philipp Zagar <zagar@stanford.edu>
2024-01-07 20:55:35 -0800

    some further progress

--- Sources/SpeziLLMOpenAI/Functions/LLMFunction.swift
+++ Sources/SpeziLLMOpenAI/Functions/LLMFunction.swift
@@ -7,14 +7,18 @@
 //
 
 import OpenAI
+import os
 import Foundation
 
-protocol LLMFunction: Codable {
+
+protocol LLMFunction {
     typealias LLMFunctionParameterSchema = JSONSchema
+    
     
     static var name: String { get }
     static var description: String { get }
-    var schema: JSONSchema { get }
+    static var schema: JSONSchema { get }
+    
     
     func execute() async throws -> String
 }
@@ -29,33 +33,15 @@ extension LLMFunction {
     }
 }
 
-
-
-struct LLMTestFunction: LLMFunction {
-    static let name: String = "test"
-    static let description: String = "testDescription"
-    var schema: LLMFunctionParameterSchema
-    
-    @Parameter var parameters: SomeType
-    
-    init(someRandomParam: String) {
-        
-    }
-    
-    func execute() async throws -> String {
-        parameters.test1
-    }
-}
-
 @propertyWrapper
-class Parameter<T: Codable>: Codable {
+class Parameter<T: Decodable> {
     private var injectedValue: T? // swiftlint:disable:this discouraged_optional_collection
     
     public var wrappedValue: T {
         guard let value = injectedValue else {
             preconditionFailure("""
-                                Tried to access @Collect for value [\(T.self)] which wasn't injected yet. \
-                                Are you sure that you are only accessing @Collect within the `Module/configure` method?
+                                Tried to access @Parameter for value [\(T.self)] which wasn't injected yet. \
+                                Are you sure that you declared the function call within the respective SpeziLLM functions and only access the @Parameter within the `LLMFunction/execute()` method?
                                 """)
         }
 
@@ -66,11 +52,11 @@ class Parameter<T: Codable>: Codable {
 }
 
 extension Parameter: ParameterValueCollector {
-    public func retrieve(from data: Data) {
+    public func retrieve(from data: Data) throws {
         // TODO: Decoding needs to happen here from the JSON to the value
         // We know the type here (Value), so decoding should be possible
         //injectedValues = repository[CollectedModuleValue<Value>.self] ?? []
-        injectedValue = try? JSONDecoder().decode(T.self, from: data)
+        injectedValue = try JSONDecoder().decode(T.self, from: data)
     }
 }
 
@@ -78,7 +64,7 @@ extension Parameter: ParameterValueCollector {
 protocol ParameterValueCollector {
     /// This method is called to retrieve all the requested values from the given ``SpeziStorage`` repository.
     /// - Parameter repository: Provides access to the ``SpeziStorage`` repository for read access.
-    func retrieve(from data: Data)
+    func retrieve(from data: Data) throws
 }
 
 
@@ -87,24 +73,72 @@ extension LLMFunction {
         retrieveProperties(ofType: ParameterValueCollector.self)
     }
 
-    func injectModuleValues(from data: Data) {
+    func injectParameterValues(from data: Data) {
+        // No injection of data
+        guard storageValueCollectors.count != 0 && Self.schema.type != .null else {
+            return
+        }
+        
+        // Ensure only one parameter property exists
+        guard storageValueCollectors.count == 1,
+              let collector = storageValueCollectors.first else {
+            preconditionFailure("""
+                                Multiple @Parameter values have been specified within the LLMFunction.
+                                Ensure that only one @Parameter value is defined that contains all relevant
+                                function calling parameters, reflected in the LLMFunctionParameterSchema.
+                                """)
+        }
+        
+        collector.retrieve(from: data)
+        
+        /*
         for collector in storageValueCollectors {
             collector.retrieve(from: data)
         }
+         */
     }
 }
 
 
-struct SomeType: Codable {
+struct SomeType: Decodable {
     let test1: String
     let test2: Int
 }
 
+struct LLMTestFunction: LLMFunction {
+    static let name: String = "test"
+    static let description: String = "testDescription"
+    static var schema: LLMFunctionParameterSchema = .init(
+        type: .object,
+        properties: [
+            "test1": .init(
+                type: .string,
+                description: "this parameter does this and that"
+            ),
+            "test2": .init(
+                type: .integer,
+                description: "this parameter does this and that"
+            )
+        ]
+    )
+    
+    static var schema2: LLMFunctionParameterSchema = .init(type: .null)
+    
+    @Parameter var parameter: SomeType
+    
+    init(someRandomParam: String) {
+        
+    }
+    
+    func execute() async throws -> String {
+        parameter.test1
+    }
+}
 
 //let functions: [any LLMFunction] = [
 // LLMTestFunction(someRandomParam: "adf")
 //]
 
 let json = "{\"test1\": \"hello\", \"test2\": 123}"
-let injectedFuc = LLMTestFunction(someRandomParam: "adf").injectModuleValues(from: json.data(using: .utf8) ?? Data())
+let injectedFuc = LLMTestFunction(someRandomParam: "adf").injectParameterValues(from: json.data(using: .utf8) ?? Data())
 print(injectedFuc)
