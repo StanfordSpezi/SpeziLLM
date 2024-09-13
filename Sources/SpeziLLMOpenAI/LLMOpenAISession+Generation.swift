StanfordSpezi/SpeziLLM

commit a1fa63606f612de767304a3380d09af0b2da214e

LLMOpenAI: address some remaining Swiftlint warnings

Sources/SpeziLLMOpenAI/LLMOpenAISession+Generation.swift
@@ -98,7 +98,7 @@ extension LLMOpenAISession {
                 return
             }
 
-            let functionCalls = llmStreamResults.values.compactMap{ return $0.functionCall }.flatMap { $0 }
+            let functionCalls = llmStreamResults.values.compactMap(\.functionCall).flatMap { $0 }
 
             // Exit the while loop if we don't have any function calls
             guard !functionCalls.isEmpty else {

Sources/SpeziLLMOpenAI/Configuration/LLMOpenAIModelParameters.swift
@@ -76,4 +76,3 @@ public struct LLMOpenAIModelParameters: Sendable {
         self.user = user
     }
 }
-

Sources/SpeziLLMOpenAI/FunctionCalling/LLMFunctionParameterSchemaCollector.swift
@@ -18,8 +18,6 @@ protocol LLMFunctionParameterSchemaCollector {
 
 
 extension LLMFunction {
-    
-    
     typealias LLMFunctionParameterSchema = Components.Schemas.FunctionParameters
     var schemaValueCollectors: [String: LLMFunctionParameterSchemaCollector] {
         retrieveProperties(ofType: LLMFunctionParameterSchemaCollector.self)

Sources/SpeziLLMOpenAI/FunctionCalling/LLMFunctionParameterWrapper+OptionalTypes.swift
@@ -151,7 +151,6 @@ extension _LLMFunctionParameterWrapper where T: AnyOptional, T.Wrapped: StringPr
                 addProp["enum"] = `enum`.map { $0.map { String($0) } }
             }
             try self.init(schema: .init(additionalProperties: .init(unvalidatedValue: addProp)))
-
         } catch {
             logger.error("SpeziLLMOpenAI - initialization error - LLMFunctionParameterWrapper+OptionalTypes")
             self.init(description: "")

Sources/SpeziLLMOpenAI/FunctionCalling/LLMFunctionParameterWrapper.swift
@@ -11,14 +11,14 @@ import OpenAPIRuntime
 
 /// Alias of the OpenAI `JSONSchema/Property` type, describing properties within an object schema.
 public typealias LLMFunctionParameterPropertySchema = Components.Schemas.FunctionParameters
-/// Alias of the OpenAI `JSONSchema/Item` type, describing array items within an array schema.
-
 // FIXME: LLMFunctionParameterItemSchema does not use a generated type yet
+
+/// Alias of the OpenAI `JSONSchema/Item` type, describing array items within an array schema.
 public typealias LLMFunctionParameterItemSchema = ChatQuery.ChatCompletionToolParam.FunctionDefinition
     .FunctionParameters.Property.Items
 
-/// Refer to the documentation of ``LLMFunction/Parameter`` for information on how to use the `@Parameter` property wrapper.
 // swiftlint:disable type_name
+/// Refer to the documentation of ``LLMFunction/Parameter`` for information on how to use the `@Parameter` property wrapper.
 @propertyWrapper
 public class _LLMFunctionParameterWrapper<T: Decodable>: LLMFunctionParameterSchemaCollector {
     // swiftlint:enable type_name
@@ -55,13 +55,9 @@ public class _LLMFunctionParameterWrapper<T: Decodable>: LLMFunctionParameterSch
     ///    - description: Describes the purpose of the parameter, used by the LLM to grasp the purpose of the parameter.
     @_disfavoredOverload
     public convenience init(description _: some StringProtocol) where T: LLMFunctionParameter {
-        do {
-            try self.init(schema: T.schema)
-        } catch {
-            fatalError("LLMFunctionPropertyWrapper")
-        }
+        self.init(schema: T.schema)
     }
-    
+
     init(schema: LLMFunctionParameterPropertySchema) {
         self.schema = schema
     }

Sources/SpeziLLMOpenAI/Onboarding/LLMOpenAIModelOnboardingStep.swift
@@ -15,14 +15,18 @@ import SwiftUI
 public struct LLMOpenAIModelOnboardingStep: View {
     public enum Default {
         public static let models: [LLMOpenAIModelType] = [
-            .init(value1: "GPT 3.5 Turbo",
-                  value2: .gpt_hyphen_3_period_5_hyphen_turbo),
+            .init(
+                value1: "GPT 3.5 Turbo",
+                value2: .gpt_hyphen_3_period_5_hyphen_turbo
+            ),
             .init(
                 value1: "GPT 4 Turbo",
                 value2: .gpt_hyphen_4_hyphen_turbo
             ),
-            .init(value1: "GPT 4O",
-                  value2: .gpt_hyphen_4o)
+            .init(
+                value1: "GPT 4O",
+                value2: .gpt_hyphen_4o
+            )
         ]
     }
     
@@ -103,4 +107,3 @@ public struct LLMOpenAIModelOnboardingStep: View {
         _modelSelection = State(initialValue: models.first ?? .init(value2: .gpt_hyphen_3_period_5_hyphen_turbo))
     }
 }
-

commit ab9951d282c62817cb78ef48bf205d0c4702b1b7

LLMOpenAI: fix "functionCalls" assignment
Fixes: a1fa63606f612de767304a3380d09af0b2da214e

Sources/SpeziLLMOpenAI/LLMOpenAISession+Generation.swift
@@ -98,7 +98,7 @@ extension LLMOpenAISession {
                 return
             }
 
-            let functionCalls = llmStreamResults.values.compactMap(\.functionCall).flatMap { $0 }
+            let functionCalls = llmStreamResults.values.compactMap { $0.functionCall }.flatMap { $0 }
 
             // Exit the while loop if we don't have any function calls
             guard !functionCalls.isEmpty else {