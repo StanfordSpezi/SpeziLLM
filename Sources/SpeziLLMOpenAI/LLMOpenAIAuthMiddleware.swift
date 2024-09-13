02f796e25b60fb1b7a68711f403ebdbd7e4fc067
Paul Heidekrüger <paul.heidekrueger@tum.de>
2024-09-13 10:26:15 -0700

    LLMOpenAI: add licence header to LLMOpenAIAuthMiddleware.swift

--- Sources/SpeziLLMOpenAI/LLMOpenAIAuthMiddleware.swift
+++ Sources/SpeziLLMOpenAI/LLMOpenAIAuthMiddleware.swift
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Spezi open source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 // Based on: https://github.com/apple/swift-openapi-generator/blob/main/Examples/auth-client-middleware-example/Sources/AuthenticationClientMiddleware/AuthenticationClientMiddleware.swift
 
 import Foundation
