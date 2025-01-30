846a5e9a6c6b83e2cf0eadac399ae70bce565254
Vishnu Ravi <vishnu.ravi@gmail.com>
2025-01-30 17:59:48 -0500

    Update Sources/SpeziLLMLocal/LLMLocalSession+Generate.swift
    Co-authored-by: Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>

--- Sources/SpeziLLMLocal/LLMLocalSession+Generate.swift
+++ Sources/SpeziLLMLocal/LLMLocalSession+Generate.swift
@@ -20,7 +20,7 @@ extension LLMLocalSession {
     // swiftlint:disable:next identifier_name function_body_length cyclomatic_complexity
     internal func _generate(continuation: AsyncThrowingStream<String, any Error>.Continuation) async {
 #if targetEnvironment(simulator)
-        return await _mockGenerate(continuation: continuation) // swiftlint:disable:this return_value_from_void_function
+       await _mockGenerate(continuation: continuation)
 #endif
         
         guard let modelContainer = await self.modelContainer else {
