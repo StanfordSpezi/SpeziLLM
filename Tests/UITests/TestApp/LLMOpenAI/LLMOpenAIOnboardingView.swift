af2d7b13095bb1de38efed23869e142552189b6f
Vishnu Ravi <vishnur@stanford.edu>
2025-04-23 22:35:35 -0700

    Fix failing test

--- Tests/UITests/TestApp/LLMOpenAI/LLMOpenAIOnboardingView.swift
+++ Tests/UITests/TestApp/LLMOpenAI/LLMOpenAIOnboardingView.swift
@@ -29,7 +29,7 @@ struct LLMOpenAIOnboardingView: View {
                     } label: {
                         Image(systemName: "xmark")
                             .font(.headline)
-                            .accessibilityLabel(content: "Cancel")
+                            .accessibilityLabel("Cancel")
                     }
                 }
             }
