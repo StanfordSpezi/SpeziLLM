d5fd41dde70935acaf08662471050e1f05e77114
Philipp Zagar <zagar@stanford.edu>
2025-08-05 20:34:53 +0200

    Update package versions and dependencies

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
     name: "SpeziLLM",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v17),
-        .visionOS(.v1),
-        .macOS(.v14)
+        .iOS(.v18),
+        .visionOS(.v2),
+        .macOS(.v15)
     ],
     products: [
         .library(name: "SpeziLLM", targets: ["SpeziLLM"]),
@@ -28,15 +28,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.25.6")),
         .package(url: "https://github.com/ml-explore/mlx-swift-examples", exact: "2.25.5"),  // Pin MLX Swift Examples as it doesn't follow semantic versioning
-        .package(url: "https://github.com/huggingface/swift-transformers", .upToNextMinor(from: "0.1.14")),
+        .package(url: "https://github.com/huggingface/swift-transformers", .upToNextMinor(from: "0.1.22")),
         .package(url: "https://github.com/StanfordSpezi/Spezi", from: "1.9.0"),
-        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation", from: "2.1.8"),
-        .package(url: "https://github.com/StanfordSpezi/SpeziStorage", from: "2.1.1"),
-        .package(url: "https://github.com/StanfordSpezi/SpeziOnboarding", exact: "2.0.0-alpha.5"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziFoundation", from: "2.2.0"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziStorage", from: "2.1.0"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziOnboarding", exact: "2.0.0-alpha.8"),
         .package(url: "https://github.com/StanfordSpezi/SpeziChat", .upToNextMinor(from: "0.2.4")),
-        .package(url: "https://github.com/StanfordSpezi/SpeziViews", from: "1.10.1"),
+        .package(url: "https://github.com/StanfordSpezi/SpeziViews", from: "1.12.0"),
         .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.8.0"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.2"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.8.0"),
         .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.1.0")
     ],
     targets: [
