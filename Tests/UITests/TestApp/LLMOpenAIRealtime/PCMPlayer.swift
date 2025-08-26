bbe65bff55983c998dc69229802120bbc6e69b0a
Sebastien Letzelter <sebastien.letzelter@gmail.com>
2025-08-26 17:43:37 +0200

    Add copyright and licensing information

--- Tests/UITests/TestApp/LLMOpenAIRealtime/PCMPlayer.swift
+++ Tests/UITests/TestApp/LLMOpenAIRealtime/PCMPlayer.swift
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Spezi open source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import AVFAudio
 import Foundation
 
